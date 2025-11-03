84dd26e5cedae2f1cae3382c6436f5c6b80e6d29
ren-wmaza <wmaza@rengroup.co>
2025-11-03 01:45:41 -0500

    Probando husky commint

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
+  console.log('hola');
+
   console.log();
 
   await app.listen(process.env.PORT ?? 3000);
